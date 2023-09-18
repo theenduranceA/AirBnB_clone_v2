e3b16a884efc8bedc9e468c0de3da8c4d6e77b8b
theenduranceA <theenduranceaneke@gmail.com>
2023-09-18 13:02:06 -0700

    Task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,9 @@
--- A test script for a MySQL server project with database hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Creates a new user new user hbnb_dev (in localhost) & password set to hbnb_dev_pwd
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+-- A test script for a MySQL server project with database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Creates a new user new user hbnb_test (in localhost) & password set to hbnb_test_pwd
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 -- Granting all priviledges to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
 -- Granting only selected priviledges to user
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
 FLUSH PRIVILEGES;
