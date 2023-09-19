424840285ab7ecb9bfdf70564592ca9410a86f8d
thorbie <jonahemmanuel54@gmail.com>
2023-09-19 18:39:24 +0100

    updating basemodel and creating database engine

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
+type_storage = os.environ.get('HBNB_TYPE_STORAGE')
 
-
-storage = FileStorage()
+if type_storage == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
