c39b78d0ef351471ca49304936bb3cd4fe760b5c
theenduranceA <theenduranceaneke@gmail.com>
2023-09-20 11:24:18 -0700

    Init

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os
-type_storage = os.environ.get('HBNB_TYPE_STORAGE')
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-if type_storage == 'db':
-    from models.engine.db_storage import DBStorage
+StorageType = getenv('HBNB_TYPE_STORAGE')
+if StorageType == 'db':
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
