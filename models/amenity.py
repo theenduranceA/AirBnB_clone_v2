f6a1c6c3c1d6efc831cb121331e2a03afd056679
theenduranceA <theenduranceaneke@gmail.com>
2023-09-20 10:37:23 -0700

    Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
-import models
+from os import getenv
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
@@ -10,5 +13,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     if storage_type == "db":
         name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+                'Place', secondary='place_amenity', back_populates="amenities")
     else:
         name = ""
