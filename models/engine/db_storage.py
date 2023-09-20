d93bc2ec2b5973c9fe6340480637479f0954eb43
theenduranceA <theenduranceaneke@gmail.com>
2023-09-20 12:38:25 -0700

    DB storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,58 +9,58 @@ from models.user import User
 from models.review import Review
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
-import models
-import os
-
-
-user = os.getenv('HBNB_MYSQL_USER')
-pwd = os.getenv('HBNB_MYSQL_PWD')
-host = os.getenv('HBNB_MYSQL_HOST')
-db = os.getenv('HBNB_MYSQL_DB')
-env = os.getenv('HBNB_ENV')
+from os import getenv
 
 
 class DBStorage:
     """defining the class DBStorage"""
 
-    __classes = [State, City, User, Place, Review, Amenity]
     __engine = None
     __session = None
 
     def __init__(self):
         """constructor for DBStorage"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            user, pwd, host, db), pool_pre_ping=True)
+        user = getenv("HBNB_MYSQL_USER")
+        pwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine(
+            f"mysql+mysqldb://{user}:{pwd}@{host}/{db}",
+            pool_pre_ping=True,
+        )
+
         if env == "test":
-            Base.metadata.drop_all()
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Method to return a dictionary of object"""
         my_dict = {}
-        if cls in self.__classes:
-            result = DBStorage.__session.query(cls)
-            for row in result:
-                key = "{}.{}".format(row.__class__.name__, row.id)
-                my_dict[key] = row
-        elif cls is None:
-            for cl in self.__classes:
-                result = DBStorage.__session.query(cl)
-                for row in result:
-                    key = "{}.{}".format(row.__class__.name__, row.id)
-                    my_dict[key] = row
-        return my_dict
+        self.__classes = [Amenity, City, User, Place, State, Review]
+        if cls is None:
+            for _class in self.__classes.values():
+                if self.__session.query(_class).all():
+                    for item in self.__session.query(_class).all():
+                        my_dict[item.id] = item
+        else:
+            for item in self.__session.query(self.__classes[cls]):
+                my_dict[item.id] = item
+        return (my_dict)
 
     def new(self, obj):
         """Method to add new object to the cuurrent database"""
-        DBStorage.__session.add(obj)
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
         """methid to commit all changes to the current database"""
-        DBStorage.__session.commit()
+        self.__session.commit()
 
     def delete(self, obj=None):
         """Method to delete an object in the current database"""
-        DBStorage.__session.delete(obj)
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
         """method to create the current database session"""
@@ -68,8 +68,8 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-        DBStorage.__session = Session()
+        self.__session = Session()
 
     def close(self):
         """Public Method to call remove method"""
-        DBStorage.__session.close()
+        self.__session.close()
