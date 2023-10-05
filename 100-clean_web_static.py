8ebefbad72802ca6e70568ff480653ec0b05d468
theenduranceA <theenduranceaneke@gmail.com>
2023-10-05 11:08:47 -0700

    Task 6

---
+++ 100-clean_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that deletes out-of-date archives,
+using the function do_clean.
+"""
+import os.path
+from fabric.api import *
+import time
+env.hosts = ["35.174.205.224", "54.172.243.100"]
+
+
+def do_clean(number=0):
+    """ Module for do_clean function."""
+
+    if number == 0 or number == 1:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +1 | xargs -d '\n' rm -rf")
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +1 | xargs -d '\n' rm -rf")
+    else:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +{} | xargs -d '\n' rm -rf".format(number))
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
