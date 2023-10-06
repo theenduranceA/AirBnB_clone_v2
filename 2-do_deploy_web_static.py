8a36d4da08ec8cd6aedabcf212338f8d88134895
theenduranceA <theenduranceaneke@gmail.com>
2023-10-05 20:48:07 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,24 +21,17 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run(
-                "sudo mkdir -p /data/web_static/releases/{}/".format(test1))
-        run(
-                "sudo tar -xzf /tmp/{} -C /data/web_static/\
-                        releases/{}/".format(test2, test1))
-        run(
-                "sudo rm /tmp/{}".format(test2))
-        run(
-                "sudo mv /data/web_static/releases/{}/web_static/*\
-                        /data/web_static/releases/{}/".format(test1, test1))
-        run(
-                "sudo rm -rf /data/web_static/releases/{}\
-                        /web_static".format(test1))
-        run(
-                "sudo rm -rf /data/web_static/current")
-        run(
-                "sudo ln -s /data/web_static/releases/{}/ \
-                        /data/web_static/current".format(test1))
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(test1))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/ \
+                releases/{}/".format(test2, test1))
+        run("sudo rm /tmp/{}".format(test2))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(test1, test1))
+        run("sudo rm -rf /data/web_static/releases/{} \
+                /web_static".format(test1))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
+                /data/web_static/current".format(test1))
 
         return True
 
