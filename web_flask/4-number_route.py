830543a59595e4a4b8c097be98e640c6739da9fe
theenduranceA <theenduranceaneke@gmail.com>
2023-10-20 05:11:42 -0700

    Task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """String to be returned"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Sub-string to display."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays C and its text."""
+    return "C %s" % text.replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays Python and its text"""
+    return "Python %s" % text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays n is a number, only if n is an integer"""
+    return "%s is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
