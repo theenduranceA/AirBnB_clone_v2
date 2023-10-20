e8a4ccf38498f6b56536371b56abb7e34023b919
theenduranceA <theenduranceaneke@gmail.com>
2023-10-20 06:05:25 -0700

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a flask web application."""
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -38,7 +38,7 @@ def number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Display a HTML page only if n is an integer"""
+    """Displays a HTML page only if n is an integer"""
     return render_template("5-number.html", n=n)
 
 
