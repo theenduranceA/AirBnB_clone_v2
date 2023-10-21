f29b0ac686d3da9968d783daf5e42a4845fc6ccd
theenduranceA <theenduranceaneke@gmail.com>
2023-10-21 07:37:42 -0700

    Basemodel updated

--- models/base_model.py
+++ models/base_model.py
@@ -11,29 +11,31 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(
-            DateTime, nullable=False, default=datetime.utcnow,
-            onupdate=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
         if kwargs:
-            for key, value in kwargs.items():
-                if key in ["created_at", "updated_at"]:
-                    date = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, date)
-                elif key != "__class__":
-                    setattr(self, key, value)
+            for key in ['updated_at', 'created_at']:
+                if key in kwargs:
+                    kwargs[key] = datetime.strptime(
+                            kwargs[key],
+                            '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs.pop('__class__', None)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls_dict = self.__dict__.copy()
+        cls_dict.pop('_sa_instance_state', None)
+        return '[{}] ({}) {}'.format(cls, self.id, cls_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
