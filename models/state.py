61919988ba521ba52c6dbee2f8c78bf93a2f7f73
theenduranceA <theenduranceaneke@gmail.com>
2023-10-21 06:18:22 -0700

    Updated state

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
+import models
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
@@ -12,20 +13,19 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-                "City", backref="state", cascade="all, delete-orphan")
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City"""
-        from models import storage
-        my_list = []
-        extracted_cities = storage.all(City)
-        for city in extracted_cities.value():
-            if city.state_id == self.id:
-                my_list.append(city)
-        return my_list
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City"""
+            my_list = []
+            extracted_cities = models.storage.all(City)
+            for city in list(extracted_cities.values()):
+                if city.state_id == self.id:
+                    my_list.append(city)
+            return my_list
