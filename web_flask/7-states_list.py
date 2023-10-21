565ad2e4f348ef413c35ea088024bf6ab051c53e
theenduranceA <theenduranceaneke@gmail.com>
2023-10-21 06:36:39 -0700

    Task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application."""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Displays html page"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
