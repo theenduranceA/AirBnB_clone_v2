8c75a88e8b0dffb426e962d3393af16a575a3948
theenduranceA <theenduranceaneke@gmail.com>
2023-10-22 19:51:48 -0700

    Pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def teardown(self):
 @app.route("/states_list", strict_slashes=False)
 def display_states():
     """Displays html page."""
-    states = storage.all()
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
